551578fb6c106b220d60f669bb69e33cefcc36bd
Bojan <bojan.colic131@gmail.com>
2026-03-18 10:47:27 +0100

    add e2e script

--- packages/e2e-tests/playwright.config.ts
+++ packages/e2e-tests/playwright.config.ts
@@ -12,6 +12,7 @@ export default defineConfig({
   reporter: [['html', { open: 'never' }], ['list']],
   use: {
     baseURL: BASE_URL,
+    headless: true,
     trace: 'on-first-retry',
     screenshot: 'only-on-failure',
     video: 'on-first-retry',
